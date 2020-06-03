0f51e599a4e2d097f886492ff793106565735c92
madhawap <madhawa.818@gmail.com>
2020-06-03 15:57:26 +1000

    Modify getAllGestures() endpoint

--- src/main/java/com/hdgi/app/Utills/OntologyQueryHandler.java
+++ src/main/java/com/hdgi/app/Utills/OntologyQueryHandler.java
@@ -7,6 +7,7 @@ import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.FileManager;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class OntologyQueryHandler {
@@ -74,5 +75,47 @@ public class OntologyQueryHandler {
             newGesture.setGestureDetails(gestureDetails);
             return newGesture;
         }
+    }
+
+    public ArrayList<String> extractAllGestures() {
+        ArrayList<String> gestures = new ArrayList<String>();
+        Model mdl = FileManager.get().loadModel("ontology/HDGI-ontology-4.ttl");
+
+        String qry =
+                "PREFIX hdgi: <https://w3id.org/hdgi#>\n" +
+                        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                        "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                        "\n" +
+                        "SELECT ?allGestures\n" +
+                        "WHERE\n" +
+                        "{\n" +
+                        "{?allGestures rdf:type hdgi:ForearmGesture  .}\n" +
+                        "UNION\n" +
+                        "{?allGestures rdf:type hdgi:UpperArmGesture  .}\n" +
+                        "UNION\n" +
+                        "{?allGestures rdf:type hdgi:HandGesture  .}\n" +
+                        "UNION\n" +
+                        "{?allGestures rdf:type hdgi:PalmGesture  .}\n" +
+                        "UNION\n" +
+                        "{?allGestures rdf:type hdgi:FingerGesture  .}\n" +
+                        "UNION\n" +
+                        "{?allGestures rdf:type hdgi:Gesture  .}\n" +
+                        "}";
+        Query query = QueryFactory.create(qry);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, mdl)) {
+            ResultSet rs = qexec.execSelect();
+//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//            ResultSetFormatter.outputAsJSON(outputStream, rs);
+//            String json = new String(outputStream.toByteArray());
+//            System.out.println(json);
+            while (rs.hasNext()) {
+                QuerySolution sol = rs.nextSolution();
+                Resource subjects = sol.getResource("allGestures");
+                gestures.add("hdgi:"+subjects.getLocalName());
+//                System.out.println(subjects.toString());
+            }
+        }
+        return gestures;
     }
 }
